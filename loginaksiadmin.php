dd23f232c6aa24e98317b2590f2d838af1ad6524
Achmad Alfin Rizaldi <assyifanornadzilla05@gmail.com>
2024-05-10 16:24:22 +0800

    Penambahan login dan pemberian hak akses admin dan mahasiswa

---
+++ loginaksiadmin.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require "koneksi.php";
+ceklogin();
+
+$user = $_POST['user'];
+$password = $_POST['password'];
+
+//query database
+$result = mysqli_query($conn, "SELECT * FROM admin WHERE user= '$user'");
+
+$row = mysqli_fetch_assoc($result);
+
+if (password_verify($password, $row['password'])) {
+    $_SESSION['login'] = true;
+    $_SESSION['nama'] = $row['user'];
+    $_SESSION['foto'] = 'admin.jpg';
+    $_SESSION['hakakses'] = 'admin';
+    header("Location: index.php");
+    
+} else {
+    echo "
+    <script>
+        alert('Username atau Password Salah');
+        document.location.href='login.php';
+    </script>
+    ";
+}
+//var_dump($row);
